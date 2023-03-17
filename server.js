5de94ca088c5144824853142fd53adc57cbc42d3
Bruno Silva Barbosa <brunobarbosa@dgtbr.com.br>
2023-03-17 17:45:25 -0400

    não serjio

--- server.js
+++ server.js
@@ -30,11 +30,6 @@ app.post('/usuario', (req, res) => {
     res.json({status: 'Usuario criado com sucesso'})
 })
 
-app.post('/serjio', (req, res) => {
-    usuario.push(req.body)
-    res.json({status: 'serjio'})
-})
-
 app.listen(8080, () => {
     console.log('rodando e http://localhost:8080')
 })
