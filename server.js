9291920a26cf93e4ed72bf669cd1fde9b96dc4e5
Bruno Silva Barbosa <brunobarbosa@dgtbr.com.br>
2023-03-21 16:00:29 -0400

    att 2.0

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 let app = express()
+const { engine } = require('express-handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
-const usuario = [
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+const usuarios = [
     
 ]
 
 app.get('/', (req, res) => {
-    res.send('entrou')
+    res.json(usuarios)
 })
 
 // app.get('/cadastro', (req, res) => {
@@ -20,15 +24,24 @@ app.get('/', (req, res) => {
 //     res.send('perfil do usuário')
 // })
 
-app.get('/usuario', (req, res) => {
-    res.json(usuario)
+app.get('/usuarios', (req, res) => {
+    res.render('usuarios')
+    // res.json(usuarios)
     console.log(req.body)
 })
 
-app.post('/usuario', (req, res) => {
-    usuario.push(req.body)
-    res.json({status: 'Usuario criado com sucesso'})
+app.post('/usuarios', (req, res) => {
+    res.send("Nome: " + req.body.nome + "\nEmail: " + req.body.email)
+    usuarios.push(req.body)
+    // usuarios.push(req.body)
+    // res.json({status: 'Usuario criado com sucesso'})
 })
+
+app.delete('/usuarios', (req, res) => {
+    usuarios.pop(req.body)
+    res.json({status: 'Usuario foi deletado com sucesso'})
+})
+
 
 app.listen(8080, () => {
     console.log('rodando e http://localhost:8080')
