5548f4951dbc03b9c78bb2cc0415f9bcaec33b6b
Bruno Silva Barbosa <brunobarbosa@dgtbr.com.br>
2023-04-20 17:34:16 -0400

    hbars

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.get('/', (req, res) => {
     res.json("usuarios")
 })
 
+app.use(function (err, req, res, next) {
+    if (err.message && (~err.message.indexOf('not found') || (~err.message.indexOf('Cast to ObjectId failed')))) {
+      return next();
+    }
+  
+    console.error(err.stack);
+  
+    res.status(500).render('500', { error: err.stack });
+  });
+
 app.get('/usuarios', async (req, res) => {
     try {
         const user = await pool.query("SELECT * FROM usuario")
